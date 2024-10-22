d7b5ac17d213e9c6e84b55d94c66ace7dcbcfe40
IvanBliudnik <Bliudnik007@gmail.com>
2024-10-22 16:42:58 +0300

    2nd

--- src/App.tsx
+++ src/App.tsx
@@ -13,18 +13,24 @@ function App() {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    <div> <NavLink to={"/page1"}>Page1</NavLink></div>
-                    <div> <NavLink to={"/page2"}>Page2</NavLink></div>
-                    <div> <NavLink to={"/page3"}>Page3</NavLink></div>
+                    <div><NavLink to={"/page1"} className={({isActive}) =>
+                        isActive ? "navLink" : "activeNavLink"
+                    }>Page1</NavLink></div>
+                    <div><NavLink to={"/page2"} className={({isActive}) =>
+                        isActive ? "navLink" : "activeNavLink"
+                    }>Page2</NavLink></div>
+                    <div><NavLink to={"/page3"} className={({isActive}) =>
+                        isActive ? "navLink" : "activeNavLink"
+                    }>Page3</NavLink>
+                    </div>
                 </div>
                 <div className={styles.content}>
                     <Routes>
-                        <Route path={"/"} element={<Navigate to={"/page1"}/>} />
+                        <Route path={"/"} element={<Navigate to={"/page1"}/>}/>
 
-                        <Route path="/page1" element={<PageOne/>} />
+                        <Route path="/page1" element={<PageOne/>}/>
                         <Route path="/page2" element={<PageTwo/>}/>
                         <Route path="/page3" element={<PageThree/>}/>
-
                         <Route path={"/page/error"} element={<Error404/>}/>
                         <Route path={"/*"} element={<Navigate to={"/page/error"}/>}/>
                     </Routes>
@@ -32,7 +38,8 @@ function App() {
             </div>
             <div className={styles.footer}>abibas 2023</div>
         </div>
-    );
+    )
+        ;
 }
 
 
